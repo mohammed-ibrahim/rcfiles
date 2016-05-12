a0c79d4aac4157cdc92a62b9c6e3005d4279fe25
Ibrahim <mohammed.ibrahim@practo.com>
2016-05-12 12:49:33 +0000

    Adding perf tracker

---
+++ env.sh
@@ -0,0 +1,4 @@
+export PERF_MYSQL_HOST="localhost"
+export PERF_MYSQL_USER="root"
+export PERF_MYSQL_PASSWORD=""
+export PERF_MYSQL_DB="report"
---
+++ perf_tracker.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+import MySQLdb
+import os
+
+"""
+Input Configuration
+"""
+host = os.environ['PERF_MYSQL_HOST']
+user = os.environ['PERF_MYSQL_USER']
+password = os.environ['PERF_MYSQL_PASSWORD']
+db = os.environ['PERF_MYSQL_DB']
+"""
+End of Input Configuration
+"""
+
+app = Flask('smprf')
+
+db = MySQLdb.connect(host, user, password, db)
+cursor = db.cursor()
+
+def wrap(text):
+    return '\'' + text + '\''
+
+def insert_values(request_id, tag, time_taken):
+    sql = 'insert into trace (tag, time_taken, request_id, entry_time) values (%s, %s, %s, %s)'
+    value = sql % (wrap(tag), wrap(time_taken), wrap(request_id), 'now()')
+    print(value)
+    cursor.execute(value)
+    db.commit()
+
+@app.route('/add',methods=['GET'])
+def main():
+    tag = request.args.get('tag')
+    time_taken = request.args.get('time_taken')
+    request_id = request.args.get('request_id')
+
+    if (tag == None):
+        return "Tag is none", 403
+    if (time_taken == None):
+        return "Time taken is none", 403
+    if (request_id == None):
+        return "Request_id is none", 403
+
+    insert_values(request_id, tag, time_taken)
+    return "", 201
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=int("9595"), debug=True)
