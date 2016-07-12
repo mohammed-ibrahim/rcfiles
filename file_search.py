10dbe9e7b6897b4ec51d8d38c499e212ce9131e4
Ibrahim <mohammed.ibrahim@practo.com>
2016-07-12 03:39:46 +0000

    Adding simple file search

---
+++ file_search.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+def search(term):
+    term = term.lower()
+    for root, directories, filenames in os.walk('./'):
+        for directory in directories:
+                #print directory   #os.path.join(root, directory) 
+                if (directory.lower().startswith(term)):
+                    print(os.path.join(root, directory))
+        for filename in filenames: 
+                #print filename #os.path.join(root,filename) 
+                if (filename.lower().startswith(term)):
+                    print(os.path.join(root,filename))
+    return
+
+
+def get_term():
+    if len(sys.argv) not in [2]:
+        print('Usage: <program> <search_string>')
+        return None
+
+    return sys.argv[1]
+
+if __name__ == '__main__':
+    term = get_term()
+    
+    if term != None:
+        search(term)
