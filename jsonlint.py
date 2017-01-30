f4c47855da11fa4f3cdac352f6d376c6e466ac3a
Ibrahim <mohammed.ibrahim@practo.com>
2017-01-30 06:13:07 +0000

    jsonlint adding order

--- jsonlint.py
+++ jsonlint.py
@@ -1,22 +1,33 @@
 import sys
 import json
 import traceback
+from collections import OrderedDict
 
-def read_stdin():
+def read_stdin(ordered_keys):
     
     try:
-        contents = json.loads(sys.stdin.read())
-        return contents
+        if ordered_keys:
+            contents = json.loads(sys.stdin.read(), object_pairs_hook=OrderedDict)
+            return contents
+
+        else:
+            contents = json.loads(sys.stdin.read())
+            return contents
+
     except Exception as e:
         print e.args
         return None
 
+ordered_keys = False
+if len(sys.argv) == 2 and sys.argv[1] == '-o':
+    ordered_keys = True
 
-parsed = read_stdin()
+
+parsed = read_stdin(ordered_keys)
+
 print ''
 if parsed is not None:
     print json.dumps(parsed, indent=4)
-    print 'Json is Valid'
 else:
     print 'Json is Invalid'
 print ''
