d4c95c780d8a10ef8d2518971da5f572976d9ecd
mohammed-ibrahim <emmi.in@hotmail.com>
2019-08-26 19:48:10 +0530

    code ahead

--- modular.py
+++ modular.py
@@ -75,7 +75,41 @@ def update_branch(arg0, arg1, arg2, arg3, arg4, arg5):
     cmd = update_branch_template % (current_user, current_branch, required_url, current_branch, current_user, current_branch, current_user, current_branch)
     print(cmd)
 
+def head(arg0, arg1, arg2, arg3, arg4, arg5):
+    head_diff = s_run_process_and_get_output('git show HEAD')
+    file_name = "%s.diff" % get_qualifier_with_ctx()
+    write_to_file(file_name, head_diff)
+    pyperclip.copy("vi %s" % file_name)
 
+def lhead(arg0, arg1, arg2, arg3, arg4, arg5):
+    lhead_diff = s_run_process_and_get_output('git diff-tree --no-commit-id --name-only -r HEAD')
+    all_lines = [line for line in lhead_diff.split("\n") if len(line) > 0]
+    gc_param = get_param(2)
+
+    if gc_param == "-gc":
+        print("\n\n%s\n\n" % (" ".join(all_lines)))
+    else:
+        print("\n\n")
+        for line in all_lines:
+            print(line)
+        print("\n\n")
+
+def git_copy(arg0, arg1, arg2, arg3, arg4, arg5):
+    process_output = s_run_process_and_get_output('git status -s')
+    files = [x[3:] for x in process_output.split("\n") if len(x.strip()) > 0]
+    param = get_param(2)
+    text = None
+    if param is None:
+        text = " ".join(files)
+    else:
+        index = int(param)
+        if index >= len(files) or index < 0:
+            print("Invalid index")
+            for i in range(len(files)):
+                print("%s :: %s" % (i, files[i]))
+        text = files[index]
+
+    pyperclip.copy(text)
 
 #  ____ ___   __  .__.__  .__  __              _____          __  .__               .___
 # |    |   \_/  |_|__|  | |__|/  |_ ___.__.   /     \   _____/  |_|  |__   ____   __| _/______
@@ -83,6 +117,42 @@ def update_branch(arg0, arg1, arg2, arg3, arg4, arg5):
 # |    |  /  |  | |  |  |_|  ||  |  \___  | /    Y    \  ___/|  | |   Y  (  <_> ) /_/ |\___ \
 # |______/   |__| |__|____/__||__|  / ____| \____|__  /\___  >__| |___|  /\____/\____ /____  >
 #                                   \/              \/     \/          \/            \/    \/
+def get_qualifier_with_ctx():
+    ctx = get_param(2)
+    if ctx is None:
+        ctx = get_cwd_name()
+    else:
+        ctx = slugify(ctx)
+
+    local_directory = pull_env_var('LOCAL_BACKUP_DIR')
+    return os.path.join(local_directory, "%s-%s" % (ctx, get_ts()))
+
+def write_to_file(file_name, content):
+    with open(file_name, "w") as file_pointer:
+        file_pointer.write(content)
+
+    print("File write complete: " + file_name)
+
+def get_ts():
+    return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%B-%d-%H-%M-%S')
+
+def slugify(text):
+    output = re.sub(r'\W+', '-', text)
+    return output.lower()
+
+def pull_env_var(key):
+    env_value = os.environ.get(key, None)
+    if env_value is None:
+        print("%s environment variable is not set" % key)
+        err_exit()
+
+    return env_value
+
+def get_cwd_name():
+    cwd = os.getcwd()
+    path_parts = cwd.split("/")
+    cwd_name = path_parts.pop().strip()
+    return cwd_name
 
 def get_repo_url():
     process_output = s_run_process_and_get_output('git config remote.origin.url')
@@ -138,7 +208,10 @@ def get_param(index):
 PRIMARY_OPERATIONS = [
     get_cmd("ag", "test command", "ic,oc,dc", hello_world),
     get_cmd("gc", "test command", "ic,oc,dc", gc),
-    get_cmd("ub", "Update Branch", "-ob", update_branch)
+    get_cmd("ub", "Update Branch Commands.", "-ob", update_branch),
+    get_cmd("head", "Save head commit patch to backup.", "-ob", head),
+    get_cmd("lhead", "List file in head commit.", "-ob", lhead),
+    get_cmd("gp", "Git status copy", "-ob", git_copy)
 ]
 
 # __________                                             ___________ __
