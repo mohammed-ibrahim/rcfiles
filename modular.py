ce46f891b03d3967e41edd25a527819d6e8a5987
mohammed-ibrahim <emmi.in@hotmail.com>
2019-08-27 10:36:32 +0530

    Adding more commands to modular

--- modular.py
+++ modular.py
@@ -24,42 +24,27 @@ NEW_LINE = "\n"
 # /_______  /__/\_ \\___  >\___  >____/ |__| |__|\____/|___|  / \____|__  /\___  >__| |___|  /\____/\____ /____  >
 #         \/      \/    \/     \/                           \/          \/     \/          \/            \/    \/
 
-def hello_world(params, arg1, arg2, arg3, arg4, arg5):
-    print("the function is called")
 
-def gc(params, arg1, arg2, arg3, arg4, arg5):
-    print("method gc is called")
+def update_branch(params, arg2, arg3, arg4, arg5, arg6):
+    update_branch_template = """
 
-update_branch_template = """
+    JENKINS :: origin/topic/%s/%s
+    REMOTE BRANCH :: %s
 
-JENKINS :: origin/topic/%s/%s
-REMOTE BRANCH :: %s
+    git branch --set-upstream-to=origin/master %s
 
-git branch --set-upstream-to=origin/master %s
+    git commit --amend
+    git commit --amend --no-edit
 
-git commit --amend
-git commit --amend --no-edit
-
-git pull
-git rebase
+    git pull
+    git rebase
 
 
-a sci &&
-git push origin :topic/%s/%s &&
-git push origin HEAD:topic/%s/%s
-"""
-
-def update_branch(params, arg1, arg2, arg3, arg4, arg5):
-    cmd = None
-    current_branch = None
-    open_branch_link = "open_branch_link"
-    if "-ob" in sys.argv:
-        cmd = open_branch_link
-
-    non_cmd_list = get_non_cmd_params()
-    if len(non_cmd_list) > 0:
-        current_branch = non_cmd_list[0]
-
+    a sci &&
+    git push origin :topic/%s/%s &&
+    git push origin HEAD:topic/%s/%s
+    """
+    current_branch = arg1
     if current_branch is None:
         current_branch = get_current_branch()
 
@@ -75,16 +60,16 @@ def update_branch(params, arg1, arg2, arg3, arg4, arg5):
     cmd = update_branch_template % (current_user, current_branch, required_url, current_branch, current_user, current_branch, current_user, current_branch)
     print(cmd)
 
-def head(params, arg1, arg2, arg3, arg4, arg5):
+def head(params, arg2, arg3, arg4, arg5, arg6):
     head_diff = s_run_process_and_get_output('git show HEAD')
     file_name = "%s.diff" % get_qualifier_with_ctx()
     write_to_file(file_name, head_diff)
     pyperclip.copy("vi %s" % file_name)
 
-def lhead(params, arg1, arg2, arg3, arg4, arg5):
+def lhead(params, arg2, arg3, arg4, arg5, arg6):
     lhead_diff = s_run_process_and_get_output('git diff-tree --no-commit-id --name-only -r HEAD')
     all_lines = [line for line in lhead_diff.split("\n") if len(line) > 0]
-    gc_param = get_param(2)
+    gc_param = arg2
 
     if gc_param == "-gc":
         print("\n\n%s\n\n" % (" ".join(all_lines)))
@@ -94,10 +79,10 @@ def lhead(params, arg1, arg2, arg3, arg4, arg5):
             print(line)
         print("\n\n")
 
-def git_copy(params, arg1, arg2, arg3, arg4, arg5):
+def git_copy(params, arg2, arg3, arg4, arg5, arg6):
     process_output = s_run_process_and_get_output('git status -s')
     files = [x[3:] for x in process_output.split("\n") if len(x.strip()) > 0]
-    param = get_param(2)
+    param = arg2
     text = None
     if param is None:
         text = " ".join(files)
@@ -110,6 +95,98 @@ def git_copy(params, arg1, arg2, arg3, arg4, arg5):
         text = files[index]
 
     pyperclip.copy(text)
+
+def open_branch(params, arg2, arg3, arg4, arg5, arg6):
+    current_branch = arg2
+    if current_branch is None:
+        current_branch = get_current_branch()
+
+    current_user_details = s_run_process_and_get_output("whoami")
+    current_user = current_user_details.split(NEW_LINE)[0]
+    required_url = "%s/commits/topic/%s/%s" % (get_repo_url(), current_user, current_branch)
+    webbrowser.open(required_url, new=0, autoraise=True)
+
+def merge_staging(params, arg2, arg3, arg4, arg5, arg6):
+    merge_staging_template = """
+    git checkout dev/staging
+    git pull origin dev/staging
+    git merge origin/topic/%s/%s --no-commit --no-ff
+    git commit
+    git push
+    """
+    branch = arg2
+    if branch is None:
+        print("Need to send branch as parameter")
+        err_exit()
+
+    print(merge_staging_template % (get_current_user(), branch))
+
+def merge_master(params, arg2, arg3, arg4, arg5, arg6):
+    merge_master_template = """
+    git checkout master
+    git pull origin master
+    git merge origin/topic/%s/%s --no-commit --no-ff
+    git commit
+    git push
+    """
+    branch = arg2
+    if branch is None:
+        print("Need to send branch as parameter")
+        err_exit()
+
+    print(merge_master_template % (get_current_user(), branch))
+
+def save_url(params, arg2, arg3, arg4, arg5, arg6):
+    url = arg2
+    if url is None:
+        print("usage: a %s <url>" % (URL['code']))
+        return
+
+    file_name = get_qualifier_with_custom_ctx("url-save", "txt")
+    req = requests.get(url)
+    write_to_file(file_name, req.content)
+    pyperclip.copy("vi %s" % file_name)
+
+def save_curl(params, arg2, arg3, arg4, arg5, arg6):
+    url = None
+    headers = {}
+    extension = "txt"
+
+    for i in range(len(params)):
+        part = params[i]
+        if part.lower() == 'curl':
+            continue
+
+        if part == "-H":
+            next_part = params[i+1]
+            parts = next_part.split(": ")
+            headers[parts[0]] = parts[1]
+
+        if part.lower() == "-e":
+            extension = params[i+1]
+
+        if part.startswith("http"):
+            url = part
+
+    if url is None:
+        print("Invalid command")
+        return
+
+    file_name = get_qualifier_with_custom_ctx("curl-save", extension)
+    req = requests.get(url, headers = headers)
+    write_to_file(file_name, req.content)
+    pyperclip.copy("vi %s" % file_name)
+
+def save_diff(params, arg2, arg3, arg4, arg5, arg6):
+    git_diff = s_run_process_and_get_output('git diff')
+    file_name = "%s.diff" % get_qualifier_with_ctx()
+    write_to_file(file_name, git_diff)
+    pyperclip.copy("vi %s" % file_name)
+
+def get_time_stamp(params, arg2, arg3, arg4, arg5, arg6):
+    fully_qualified_path_for_backup = get_qualifier_with_ctx()
+    pyperclip.copy(fully_qualified_path_for_backup)
+    print("\n\n%s - copied to clipboard\n\n" % fully_qualified_path_for_backup)
 
 #  ____ ___   __  .__.__  .__  __              _____          __  .__               .___
 # |    |   \_/  |_|__|  | |__|/  |_ ___.__.   /     \   _____/  |_|  |__   ____   __| _/______
@@ -128,6 +205,11 @@ def get_qualifier_with_ctx():
 
     local_directory = pull_env_var('LOCAL_BACKUP_DIR')
     return os.path.join(local_directory, "%s-%s" % (ctx, get_ts()))
+
+def get_qualifier_with_custom_ctx(ctx, extension):
+    ctx = slugify(ctx)
+    local_directory = pull_env_var('LOCAL_BACKUP_DIR')
+    return os.path.join(local_directory, "%s-%s.%s" % (ctx, get_ts(), extension))
 
 def write_to_file(file_name, content):
     with open(file_name, "w") as file_pointer:
@@ -229,12 +311,17 @@ def get_params():
 if __name__ == "__main__":
 
     primary_operations = [
-        get_cmd("ag", "test command", "ic,oc,dc", hello_world),
-        get_cmd("gc", "test command", "ic,oc,dc", gc),
-        get_cmd("ub", "Update Branch Commands.", "-ob", update_branch),
-        get_cmd("head", "Save head commit patch to backup.", "-ob", head),
-        get_cmd("lhead", "List file in head commit.", "-ob", lhead),
-        get_cmd("gp", "Git status copy", "-ob", git_copy)
+        get_cmd("ub", "Update Branch Commands.", "non", update_branch),
+        get_cmd("head", "Save head commit patch to backup.", "non", head),
+        get_cmd("lhead", "List file in head commit.", "non", lhead),
+        get_cmd("gp", "Git status copy", "-ob", git_copy),
+        get_cmd("ob", "Open Branch", "non", open_branch),
+        get_cmd("ms", "Merge into staging", "non", merge_staging),
+        get_cmd("mm", "Merge into master", "non", merge_master),
+        get_cmd("url", "Merge into master", "non", save_url),
+        get_cmd("curl", "Merge into master", "non", save_curl),
+        get_cmd("diff", "Save git diff", "non", save_diff),
+        get_cmd("ts", "Get backup time stamp", "non", get_time_stamp)
     ]
 
     primary_operation_codes = [x['code'] for x in primary_operations]
@@ -246,4 +333,4 @@ if __name__ == "__main__":
     index = primary_operation_codes.index(mode)
     arg = primary_operations[index]
     fn = arg['fnc']
-    fn(get_params(), None, None, None, None, None)
+    fn(get_params(), get_param(2), get_param(3), None, None, None)
