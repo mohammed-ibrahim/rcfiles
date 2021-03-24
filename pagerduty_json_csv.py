d1bc1bc9348de70c0f136e9ee4739912f2124717
mohammed-ibrahim <emmi.in@hotmail.com>
2021-03-24 19:09:53 +0530

    Adding json to csv converter

---
+++ pagerduty_json_csv.py
@@ -0,0 +1,72 @@
+import csv
+import json
+import sys
+import os
+import uuid
+import pyperclip
+
+
+def get_param(index):
+    if len(sys.argv) > index:
+        return sys.argv[index]
+
+    return None
+
+def read_file_contents(file_path):
+    contents = None
+    with open(file_path, "r") as file_pointer:
+        contents = file_pointer.read()
+
+    return contents
+
+
+def get_row_from_incident(i):
+    title = i['title']
+    service = i['service']['summary']
+    created_at = i['created_at']
+    assigned_via = i['assigned_via']
+    id = i['id']
+    last_update_by = i['last_status_change_by']['summary']
+
+    return [title, service, created_at, assigned_via, id, last_update_by]
+
+
+def write_json_file_entries_to_csv(full_json_file_path, csv_writer):
+    string_json = read_file_contents(full_json_file_path)
+    data = json.loads(string_json)
+
+    incidents = data['incidents']
+
+    if len(incidents) < 1:
+        return
+
+    for i in incidents:
+        row = get_row_from_incident(i)
+        csv_writer.writerow(row)
+
+
+def process_directory(directory):
+    json_files = [f for f in os.listdir(directory) if
+                  (os.path.isfile(os.path.join(directory, f)) and f.endswith(".json"))]
+    print(json_files)
+
+    csv_file_name = os.path.join(directory, str(uuid.uuid4()) + ".csv")
+    with open(csv_file_name, 'w') as writer_file_handle:
+        csv_writer = csv.writer(writer_file_handle)
+
+        for json_file in json_files:
+            full_json_file_path = os.path.join(directory, json_file)
+            write_json_file_entries_to_csv(full_json_file_path, csv_writer)
+
+    cmd = 'open -a "Microsoft Excel" %s' % csv_file_name
+    pyperclip.copy(cmd)
+    print(cmd + " :: copied to clipboard")
+
+if __name__ == "__main__":
+    directory = get_param(1)
+
+    if directory is None:
+        print("This operation requires directory")
+        sys.exit(1)
+
+    process_directory(directory)
